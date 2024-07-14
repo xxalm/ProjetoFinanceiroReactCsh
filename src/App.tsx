14c9a2e4d2da216878e91bf34b714f76dfc2431d
xxal <leckalmeida@gmail.com>
2024-07-13 23:50:19 -0300

    :tada: primeiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { ChangeEvent, SyntheticEvent, useState } from 'react';
 import './App.css';
 import CardList from './Componentes/CardList/CardList';
 import Search from './Componentes/Search/Search';
 
 function App() {
+  const [search, setSearch] = useState<string>("")
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+    console.log(e);
+  };
+
+  const onClick = (e: SyntheticEvent) => {
+    console.log(e);
+  };
   return (
     <div className="App">
-      <Search />
+      <Search onClick={onClick} search={search} handleChange={handleChange} />
       <CardList />
     </div>
   );
--- src/Componentes/Search/Search.tsx
+++ src/Componentes/Search/Search.tsx
@@ -1,16 +1,17 @@
-import React, { ChangeEvent, useState, SyntheticEvent } from 'react';
+import React, { ChangeEvent, useState, SyntheticEvent, HtmlHTMLAttributes } from 'react';
 
-type Props = {}
+interface Props {
+    onClick: (e: SyntheticEvent) => void;
+    search: string | undefined;
+    handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+};
 
-const Search: React.FC<Props> = (props: Props): JSX.Element => {
-    const [search, setSearch] = useState<string>("")
-
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setSearch(e.target.value);
-        console.log(e);
-    };
-
-    const onClick = (e: SyntheticEvent) => {};
+const Search: React.FC<Props> = ({
+    onClick, 
+    search, 
+    handleChange,
+}: Props): JSX.Element => {
+    
     return (
         <div>
             <input value={search} onChange={(e) => handleChange(e)}></input>
